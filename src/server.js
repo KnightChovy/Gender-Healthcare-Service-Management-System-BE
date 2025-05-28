4d293dd615d3aac308a099fd4fb3990d2dd03350
phucdhcoder19 <dhphuc1406@gmail.com>
2025-05-28 08:20:10 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 import express from 'express'
-
+import cors from 'cors'
 const app = express()
 
 const hostname = '0.0.0.0'
@@ -8,8 +8,8 @@ const port = 3000
 app.get('/', (req, res) => {
   res.end('<h1>Hello World!</h1><hr>')
 })
-
+app.use(cors())
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  console.log(`I am running at ${ hostname }:${ port }/`)
 })
