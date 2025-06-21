8a2dc0728b17c0be89d6ffbe23c99287805ef4f0
phucdhcoder19 <dhphuc1406@gmail.com>
2025-06-21 10:46:20 +0700

    fix service test

--- src/services/serviceService.js
+++ src/services/serviceService.js
@@ -3,6 +3,7 @@ import { MODELS } from '~/models/initModels';
 const getAllServices = async () => {
   try {
     const services = await MODELS.ServiceTestModel.findAll();
+    console.log('services', services)
     return services;
   } catch (error) {
     throw new Error('Failed to get all services: ' + error.message);
