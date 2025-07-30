5060a0666809088e4c0def35e18a2de7c5772539
khiembadinh987@gmail.com <khiembadinh987@gmail.com>
2025-07-31 04:23:09 +0700

    tra ve nhu cu

--- src/routes/v1/adminRoute.js
+++ src/routes/v1/adminRoute.js
@@ -1,17 +1,17 @@
-import express from "express";
-import { adminController } from "~/controllers/adminController";
-import isAdmin from "~/middlewares/isAdminMiddleware";
-import isAuth from "~/middlewares/isAuthMiddleware";
-import { validateCreateStaff } from "~/validations/userValidation";
+import express from 'express';
+import { adminController } from '~/controllers/adminController';
+import isAdmin from '~/middlewares/isAdminMiddleware';
+import isAuth from '~/middlewares/isAuthMiddleware';
+import { validateCreateStaff } from '~/validations/userValidation';
 const Router = express.Router();
 
-Router.route("/createStaff").post(
+Router.route('/createStaff').post(
   isAuth,
   isAdmin,
   validateCreateStaff,
   adminController.createStaff
 );
-Router.route("/deleteStaff").patch(
+Router.route('/deleteStaff').patch(
   isAuth,
   isAdmin,
   adminController.deleteStaff
